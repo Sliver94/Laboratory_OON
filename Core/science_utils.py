f827c484e30408923fae1243b09eb4998dd2a37a
Sliver94 <s277909@studenti.polito.it>
2021-03-02 16:02:32 +0100

    lab9: bit rate functions corrected, results updated

--- Core/science_utils.py
+++ Core/science_utils.py
@@ -4,6 +4,8 @@ from Core.utils import BERt
 from Core.utils import Rs
 from Core.utils import Bn
 
+from scipy import special
+
 
 def linear_to_db_conversion(x):
     return 10 * np.log10(x)
@@ -18,18 +20,18 @@ def db_to_linear_conversion(x):
 
 
 def fixed_rate_condition(x):
-    if x >= 2 * (2 * BERt) * (Rs / Bn):
+    if x >= 2 * (special.erfcinv(2 * BERt) ** 2) * (Rs / Bn):
         return 100
     else:
         return 0
 
 
 def flex_rate_condition(x):
-    if x < 2 * (2 * BERt) * (Rs / Bn):
+    if x < 2 * (special.erfcinv(2 * BERt) ** 2) * (Rs / Bn):
         return 0
-    elif x < (14 / 3) * ((3 / 2) * BERt) * (Rs / Bn):
+    elif x < (14 / 3) * (special.erfcinv((3 / 2) * BERt) ** 2) * (Rs / Bn):
         return 100
-    elif x < 10 * ((8 / 3) * BERt) * (Rs / Bn):
+    elif x < 10 * (special.erfcinv((8 / 3) * BERt) ** 2) * (Rs / Bn):
         return 200
     else:
         return 400
